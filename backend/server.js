2fd05c8a4ed59809d0de4edddfeacbc55e1e9b3b
Alan Simpson <asimpson@rrc.ca>
2022-06-17 10:36:33 -0500

    Add mongoose, MongoDB Atlas Coll.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-require('dotenv').config()
+dotenv = require('dotenv').config()
+
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 //  express app
@@ -17,8 +19,17 @@ app.use((req, res, next) => {
 //  routes
 app.use('/api/workouts/', workoutRoutes)
 
-//  listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port ', process.env.PORT)
-})
+//  connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //  listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port ', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
 
